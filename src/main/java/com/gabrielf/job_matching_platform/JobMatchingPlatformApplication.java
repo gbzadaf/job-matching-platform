ec323274a930bb6eb3c19ba2b3824b83de13e780
gbzadaf <fonsecagb23@gmail.com>
2026-09-12 17:54:46 -0300

    first commit

---
+++ src/main/java/com/gabrielf/job_matching_platform/JobMatchingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielf.job_matching_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMatchingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobMatchingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielf/job_matching_platform/JobMatchingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielf.job_matching_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMatchingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
